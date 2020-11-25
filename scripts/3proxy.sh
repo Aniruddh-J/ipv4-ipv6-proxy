31b7e4d77b13a7e595db7354cf1f880a91dcb018
xlandgroup <webxland@gmail.com>
2020-11-26 01:08:13 +0200

    Update 3proxy.sh

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -3,6 +3,7 @@ install_3proxy() {
     URL="https://github.com/z3APA3A/3proxy/archive/0.9.2.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.9.2
+    yum install make -y
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
