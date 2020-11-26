16c0e17d8d182189679b59fbcaff11f190542183
xlandgroup <webxland@gmail.com>
2020-11-26 02:00:32 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,7 +21,8 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     mv /3proxy/3proxy-0.9.2/bin/3proxy /usr/local/etc/3proxy/bin/
-    mv /3proxy/3proxy-0.9.2/scripts/3proxy.service /usr/lib/systemd/system/
+    wget https://raw.githubusercontent.com/xlandgroup/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.2/scripts/3proxy.service2
+    mv /3proxy/3proxy-0.9.2/scripts/3proxy.service2 /usr/lib/systemd/system/
     systemctl daemon-reload
     systemctl enable 3proxy
     cd $WORKDIR
